2ba3b4ddf107cae71c842d2ec11f8340bfa6fc62
Lucas Colaco <lucascolaco18@gmail.com>
2020-07-22 15:12:38 -0300

    Modified class SellerDaoJDBC

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -98,22 +98,25 @@ public class SellerDaoJDBC implements SellerDao{
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		try {
-			st = conn.prepareStatement("SELECT seller.*,department.Name as DepName " + 
-					"FROM seller INNER JOIN department " + 
-					"ON seller.DepartmentId = department.Id " + 
-					"WHERE DepartmentId = ? " + 
-					"ORDER BY Name");
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE DepartmentId = ? "
+					+ "ORDER BY Name");
 			
 			st.setInt(1, department.getId());
+			
 			rs = st.executeQuery();
 			
 			List<Seller> list = new ArrayList<>();
 			Map<Integer, Department> map = new HashMap<>();
 			
-			if(rs.next()) {
+			while (rs.next()) {
 				
 				Department dep = map.get(rs.getInt("DepartmentId"));
-				if(dep == null) {
+				
+				if (dep == null) {
 					dep = instantiateDepartment(rs);
 					map.put(rs.getInt("DepartmentId"), dep);
 				}
@@ -122,20 +125,13 @@ public class SellerDaoJDBC implements SellerDao{
 				list.add(obj);
 			}
 			return list;
-			
 		}
-		catch(SQLException e) {
+		catch (SQLException e) {
 			throw new DbException(e.getMessage());
 		}
 		finally {
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
 		}
-		
-	}
-	
-	
-	
-	
-
+}
 }
